54df3ae01f2b6decfcab1a5fd532a12057a349c3
hanjoes <hanzhou87@gmail.com>
2016-07-06 12:56:32 +0800

    done with hw05

--- hw5/HW05.hs
+++ hw5/HW05.hs
@@ -1,11 +1,15 @@
 {-# OPTIONS_GHC -Wall #-}
 {-# LANGUAGE OverloadedStrings, RecordWildCards #-}
+
 module HW05 where
 
 import Data.ByteString.Lazy (ByteString)
 import Data.Map.Strict (Map)
 import System.Environment (getArgs)
 
+import qualified Data.Function as F
+import qualified Data.List as L
+import qualified Data.Bits as B
 import qualified Data.ByteString.Lazy as BS
 import qualified Data.Map.Strict as Map
 
@@ -13,43 +17,110 @@ import Parser
 
 -- Exercise 1 -----------------------------------------
 
+extractKey :: ByteString -> ByteString -> ByteString
+extractKey oBS mBS = BS.pack $ filter (/=0) $ BS.zipWith B.xor oBS mBS
+
 getSecret :: FilePath -> FilePath -> IO ByteString
-getSecret = undefined
+getSecret oFP mFP = do
+    oBS <- BS.readFile oFP
+    mBS <- BS.readFile mFP
+    return $ extractKey oBS mBS
 
 -- Exercise 2 -----------------------------------------
 
 decryptWithKey :: ByteString -> FilePath -> IO ()
-decryptWithKey = undefined
+decryptWithKey k path = do
+    encStr <- BS.readFile $ path ++ ".enc"
+    let decStr = BS.zipWith B.xor (BS.cycle k) encStr
+    BS.writeFile path $ BS.pack decStr
 
 -- Exercise 3 -----------------------------------------
 
 parseFile :: FromJSON a => FilePath -> IO (Maybe a)
-parseFile = undefined
+parseFile path = do
+    jsonStr <- BS.readFile path
+    return $ decode jsonStr
 
 -- Exercise 4 -----------------------------------------
 
+filterTrxns :: Maybe [TId] -> Maybe [Transaction] -> Maybe [Transaction]
+filterTrxns Nothing ts = ts
+filterTrxns _ Nothing = Nothing
+filterTrxns (Just tids) (Just ts) = Just $ filter isBad ts 
+    where isBad (Transaction {tid = tid}) = tid `elem` tids
+
 getBadTs :: FilePath -> FilePath -> IO (Maybe [Transaction])
-getBadTs = undefined
+getBadTs viPath tsPath = do
+    vList <- parseFile viPath
+    tList <- parseFile tsPath
+    return $ filterTrxns vList tList
 
 -- Exercise 5 -----------------------------------------
 
 getFlow :: [Transaction] -> Map String Integer
-getFlow = undefined
+getFlow = Map.fromListWith (+) . concat . map flatten
+    where flatten (Transaction f t amt _) = [(f, -amt), (t, amt)]
 
 -- Exercise 6 -----------------------------------------
 
 getCriminal :: Map String Integer -> String
-getCriminal = undefined
+getCriminal = fst . L.maximumBy (compare `F.on` snd) . Map.toList
 
 -- Exercise 7 -----------------------------------------
 
+type FlowInfo = (String, Integer)
+
+-- Helper function to separate flow into payers and payees
+separate :: [FlowInfo] -> ([FlowInfo], [FlowInfo])
+separate [] = ([], [])
+separate l = foldr sep ([], []) l
+    where sep info (payers, payees)
+              | snd info > 0 = (info:payers, payees)
+              | snd info < 0 = (payers, info:payees)
+              | otherwise = (payers, payees)
+
+-- Helper function to sort payers and payees in descending order
+sortFlows :: ([FlowInfo], [FlowInfo]) -> ([FlowInfo], [FlowInfo])
+sortFlows (l, l') = (reverse $ L.sortBy (compare `F.on` snd) l,
+                     L.sortBy (compare `F.on` snd) l')
+
+-- Helper funciton to generate new transactions to compensate loss and gain
+compensate :: ([FlowInfo],[FlowInfo]) -> [TId] -> [Transaction]
+compensate ([], _) _ = [] 
+compensate (_, []) _ = []
+compensate (_, _) [] = []
+compensate (f:fs, f':fs') (i:is) = (gen f f' i):(compensate (fs, fs') is)
+    where gen payer payee tid =  Transaction { from = fst payer,
+                                               to = fst payee,
+                                               amount = min (snd payer) ((abs . snd) payee),
+                                               tid = tid }
+
+-- Helper function to update the flow using generated transactions
+update :: Transaction -> Map String Integer -> Map String Integer
+update t m = updatePayee t $ updatePayer t m
+
+updatePayer :: Transaction -> Map String Integer -> Map String Integer
+updatePayer (Transaction { from = name, amount = amt }) = Map.update f name
+    where f v = Just $ v - amt
+
+updatePayee :: Transaction -> Map String Integer -> Map String Integer
+updatePayee (Transaction { to = name, amount = amt }) = Map.update f name
+    where f v = Just $ v + amt
+
+-- Recover the transactions
 undoTs :: Map String Integer -> [TId] -> [Transaction]
-undoTs = undefined
+undoTs m tids = let flows = (sortFlows . separate $ Map.toList m) in
+                    case flows of
+                        ([], _) -> []
+                        (_, []) -> []
+                        fl -> let ts = compensate fl tids in
+                                  ts ++ undoTs (L.foldr update m ts) (drop (length ts) tids)
+                    
 
 -- Exercise 8 -----------------------------------------
 
 writeJSON :: ToJSON a => FilePath -> a -> IO ()
-writeJSON = undefined
+writeJSON path obj = BS.writeFile path $ encode obj
 
 -- Exercise 9 -----------------------------------------
 
@@ -66,7 +137,8 @@ doEverything dog1 dog2 trans vict fids out = do
       case mids of
         Nothing  -> error "No ids"
         Just ids -> do
-          let flow = getFlow ts       
+          let flow = getFlow ts
+          putStrLn $ show $ (sortFlows . separate) $ Map.toList (L.foldr update flow $ compensate ((sortFlows . separate) $ Map.toList flow) (repeat ""))
           writeJSON out (undoTs flow ids)
           return (getCriminal flow)
 

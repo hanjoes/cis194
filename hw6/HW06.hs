fcd57f8e2af43a7605ac74103542a007679ffb0c
hanjoes <hanzhou87@gmail.com>
2016-07-08 17:32:19 +0800

    done with ex3~9

--- hw6/HW06.hs
+++ hw6/HW06.hs
@@ -1,4 +1,6 @@
 {-# OPTIONS_GHC -Wall #-}
+{-# LANGUAGE BangPatterns #-}
+
 module HW06 where
 
 import Data.List
@@ -29,56 +31,65 @@ instance Show a => Show (Stream a) where
              ++ ",..."
 
 streamToList :: Stream a -> [a]
-streamToList = undefined
+streamToList (Cons x xs) = x:streamToList xs 
 
 -- Exercise 4 -----------------------------------------
 
 instance Functor Stream where
-    fmap = undefined
+    fmap f (Cons x xs) = Cons (f x) (fmap f xs)
 
 -- Exercise 5 -----------------------------------------
 
 sRepeat :: a -> Stream a
-sRepeat = undefined
+sRepeat x = Cons x (sRepeat x)
 
 sIterate :: (a -> a) -> a -> Stream a
-sIterate = undefined
+sIterate f s = Cons s $ sIterate f $ f s
 
 sInterleave :: Stream a -> Stream a -> Stream a
-sInterleave (Cons _ _) _ = undefined
+sInterleave (Cons x xs) s = Cons x $ sInterleave s xs
 
 sTake :: Int -> Stream a -> [a]
-sTake = undefined
+sTake n (Cons x xs)
+    | n <= 0 = []
+    | otherwise = x:sTake (n-1) xs
 
 -- Exercise 6 -----------------------------------------
 
 nats :: Stream Integer
-nats = undefined
+nats = sIterate (+1) 0
 
+-- In acc, (sRepeat 0) is just some dummy value
+-- since foldr will never use them for infinite list
 ruler :: Stream Integer
-ruler = undefined
+ruler = foldr step acc [0..]
+    where step a b = sInterleave (sRepeat a) b
+          acc = sInterleave (sRepeat 0) $ sRepeat 0
 
 -- Exercise 7 -----------------------------------------
 
 -- | Implementation of C rand
 rand :: Int -> Stream Int
-rand = undefined
+rand s = sIterate (\x -> (1103515245 * x + 12345) `mod` 2147483648) s
 
 -- Exercise 8 -----------------------------------------
 
-{- Total Memory in use: ??? MB -}
+{- Total Memory in use: 236 MB -}
 minMaxSlow :: [Int] -> Maybe (Int, Int)
 minMaxSlow [] = Nothing   -- no min or max if there are no elements
 minMaxSlow xs = Just (minimum xs, maximum xs)
 
 -- Exercise 9 -----------------------------------------
 
-{- Total Memory in use: ??? MB -}
+{- Total Memory in use: 1 MB -}
 minMax :: [Int] -> Maybe (Int, Int)
-minMax = undefined
+minMax [] = Nothing
+minMax xs = foldl' step (Just (maxBound, minBound)) xs
+    where step (Just (!mi, !ma)) n = Just (min mi n, max ma n)
+          step _ _ = Nothing
 
 main :: IO ()
-main = print $ minMaxSlow $ sTake 1000000 $ rand 7666532
+main = print $ minMax $ sTake 1000000 $ rand 7666532
 
 -- Exercise 10 ----------------------------------------
 
